74271d6f008928de076c9ead51986422bbe13c0e
kctech <kctech@kctech-PC>
2017-07-06 13:47:58 +0530

    hello commit

---
+++ Object Repository/helloobj1.rs
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>helloobj1</name>
+   <tag></tag>
+   <elementGuidId>34f18fbc-69e5-4929-a347-56346c3257d3</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/helloobj2.rs
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>helloobj2</name>
+   <tag></tag>
+   <elementGuidId>3d10d04b-28ab-4e57-a912-394a3645a83b</elementGuidId>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>ddddd</value>
+   </webElementProperties>
+</WebElementEntity>
